a746021d7a843da0e74dfef4aa8fbb951d17f00f
Dosssik <dosssik@ya.ru>
2019-03-10 22:41:53 +0500

    Search implemented

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,20 +3,52 @@ import 'package:give_me_visa/db/database.dart';
 import 'package:give_me_visa/screens/country_description.dart';
 import 'package:give_me_visa/entity/country.dart';
 
+// ROAD MAP
+// DONE - Кешировать страны после доставния из базы
+// - Прикрутить фильтры
+// - Поправить верстку на экране с подробностями
+// - Прикрутить оставшующся инфу на экран с подробностями
+// DONE - Поиск
+// - Алерт диалоги
+// - Обновить БД
+// - Запустить на iOS
+// - Разобраться с публикацией iOS
+// - РЕЛИЗ!
+
 class CountriesListRoute extends StatefulWidget {
   @override
   _CountriesListRouteState createState() => _CountriesListRouteState();
 }
 
 class _CountriesListRouteState extends State<CountriesListRoute> {
+  var _searchIcon = new Icon(Icons.search);
+  Widget _appBarTitle = Text("Дайте визу");
+  final TextEditingController _filter = new TextEditingController();
+  String searchText = "";
+
+  _CountriesListRouteState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        searchText = "";
+        setState(() {});
+      } else {
+        searchText = _filter.text;
+        setState(() {});
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Дайте визу"),
+        title: _appBarTitle, // TODO Сделать файлик со стрингами
+        actions: <Widget>[
+          IconButton(icon: _searchIcon, onPressed: _searchPressed)
+        ],
       ),
       body: FutureBuilder<List<Country>>(
-          future: DBProvider.db.getCountries(),
+          future: DBProvider.db.getCountries(searchText),
           builder:
               (BuildContext context, AsyncSnapshot<List<Country>> snapshot) {
             if (snapshot.hasData) {
@@ -58,5 +90,21 @@ class _CountriesListRouteState extends State<CountriesListRoute> {
             }
           }),
     );
+  }
+
+  _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          decoration: new InputDecoration(
+              prefixIcon: new Icon(Icons.search), hintText: 'Поиск страны...'),
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text('Дайте визу');
+      }
+    });
   }
 }
